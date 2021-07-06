97b6c3719c2e31c0cf982148cf2ba0cade5e30b7
yForecasting <yves.sagaert@gmail.com>
2021-07-06 09:00:25 +0200

    update build en license

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     name='grafton',
     license="GNU GPLv3",
     description='grafton anonymises data in any input file using an encoding key and informed consent list.',
-    version='v0.1.4',
+    version='v0.1.5',
     long_description='This package grafton can anonymise different input files such as CSV, json, XML, ... It handles the files to read/write as a flat file. In line with the GDPR legislation, the required fields are anonimised, so that any further tracking of the subjects is prevented. The key of this anonimisation process is provided in a separate file (CSV) and should be securely stored afterwards. In line with GDPR, only records with informed consent are retained. The approval of consent can be provided in a separate CSV file. The use of this package does not guarantee GDPR compliance. This package performs only the steps described above.',
     url='https://github.com/yForecasting/grafton',
     packages=setuptools.find_packages(),
@@ -22,7 +22,7 @@ setuptools.setup(
     # Enable install requires when publishing on the normal PyPi
     classifiers=[
         'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
